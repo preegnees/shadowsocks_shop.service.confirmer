1688a97cdb8bf8e3ba745e217e6ff192cb9cb628
preegnees <mrraziapov13@gmail.com>
2022-08-26 23:05:34 +0300

    прописал подключение к серверу БД с огрничением времени подключения

--- pkg/service/db/mongo/mongo.go
+++ pkg/service/db/mongo/mongo.go
@@ -1,6 +1,14 @@
 package mongo
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	mongo "go.mongodb.org/mongo-driver/mongo"
+	options "go.mongodb.org/mongo-driver/mongo/options"
+
 	models "confirmer/pkg/models"
 )
 
@@ -8,18 +16,53 @@ type Mongo struct {
 	config models.CnfDB
 }
 
-func NewMongo(cnf models.CnfDB) *Mongo {
-	var mongo = Mongo{
-		config: cnf,
+var collection *mongo.Collection
+var ctxWork = context.Background()
+
+func NewMongo(cnf models.CnfDB) (*Mongo, error) {
+	ctxInit, cancel := context.WithCancel(context.Background())
+	log.Printf("Вызов функции NewMongo с параметрами конфигурации: %v\n", cnf)
+	clientOptions := options.Client().ApplyURI(fmt.Sprintf("mongodb://localhost:%s/", string(cnf.Port)))
+	chOk := make(chan struct{})
+	go func(ch chan struct{}) {
+		for {
+			select {
+			case <- time.After(time.Duration(cnf.TimeConnect) * time.Second):
+				cancel()
+			case <- ch:
+				break
+			}
+		}
+	}(chOk)
+	client, err := mongo.Connect(ctxInit, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("$Ошибка при подключении к MonogDB, err:=%v", err)
 	}
-	// тут нужно получить клиент и тд
-	return &mongo
+	log.Println("Успешное создание клиента Монго")
+	err = client.Ping(ctxInit, nil)
+  	if err != nil {
+    	return nil, fmt.Errorf("$Ошибка при пинге MongoDB, err:=%v", err)
+  	}
+	log.Println("Успешный пинг монго")
+	collection = client.Database(string(cnf.NameDB)).Collection(string(cnf.NameDB))
+	chOk <- struct{}{}
+	return &Mongo{}, nil
 }
 
 var _ models.IStorage = (*Mongo)(nil)
 
-func (m *Mongo) Put(id models.Id, label models.Label) error 
+func (m *Mongo) Put(doc models.SaveToStorage) error {
+	_, err := collection.InsertOne(ctxWork, doc)
+	if err != nil {
+		return fmt.Errorf("$Ошибка при добавилении данных, doc=%v, err:=%v", doc, err)
+	}
+	return nil
+}
 
-func (m *Mongo) Delete(id models.Id, label models.Label) error
+func (m *Mongo) Delete(s models.SaveToStorage) error {
+	return nil
+}
 
-func (m *Mongo) Update(id models.Id, label models.Label) error
+func (m *Mongo) Update(s models.SaveToStorage) error {
+	return nil
+}

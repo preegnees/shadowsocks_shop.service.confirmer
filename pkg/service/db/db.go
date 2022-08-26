c0a10b3976bdd9053299ca88f6aaa21637ea812f
preegnees <mrraziapov13@gmail.com>
2022-08-26 23:04:07 +0300

    доавбил обработку ошибки

--- pkg/service/db/db.go
+++ pkg/service/db/db.go
@@ -1,10 +1,16 @@
 package db
 
 import (
+	"fmt"
+
 	models "confirmer/pkg/models"
 	mongo "confirmer/pkg/service/db/mongo"
 )
 
-func NewStorage(cnf models.CnfDB) models.IStorage {
-	return mongo.NewMongo(cnf)
+func NewStorage(cnf models.CnfDB) (models.IStorage, error) {
+	storage, err := mongo.NewMongo(cnf)
+	if err != nil {
+		return nil, fmt.Errorf("$Ошибка при получении хранилища, err:=%v", err)
+	}
+	return storage, nil
 }
